337af4fad79fef18e1a97adc5117cf986d33295e
Umera <umeramalek796@gmail.com>
2021-09-30 21:19:52 -0700

    js script

--- homework/Develop/script.js
+++ homework/Develop/script.js
@@ -1,19 +1,76 @@
 // moment. format to display the current day text at the header
-
-
 var currentDayText = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(currentDayText);
 
+// moment. format to display current time
 var currentTimeText = function (){
-    let currentTime = moment().format('h:mm:ss')
-    $("#currentTime").text(currentTime)
+    let currentTimeText = moment().format('h:mm:ss')
+    $("#currentTime").text(currentTimeText)
 }
-
+// call upon the current time and set interval to make it dynamic
 currentTimeText();
 setInterval(currentTimeText,1000);
 
 
+// calling upon elements
+var saveBtn = $(".saveBtn");
 
+// function to present, past and future color coded 
+function colorcode() {
+    // need to call upon current time to compare
+    var currHour = moment().hour(); 
+
+    // loop for time blocks
+    $(".timeblocks").each(function() {
+        var currTimeBlock = parseInt($(".timeblocks").attr("id"));
+
+        if (currTimeBlock > currHour) {
+            $(this).css({"color" :"red"}).addClass("future");
+        } else if (currTimeBlock === currHour) {
+            $(this).css({"color" :"green"}).addClass("present");
+        } else {
+            $(this).css({"color" :"grey"}).addClass("past");
+        }
+})
+}
+
+// when clicked on the save btn 
+saveBtn.on("click", function() {
+    // console.log(this); works
+    var hour = $(this).siblings(".hour").text();
+    var description = $(this).siblings(".description").val();
+
+    // the text in the text area is saved in the storage
+    localStorage.setItem(hour, description);
+    // console.log(hour);
+    // console.log("description");
+})
+
+// need to get item and stay after refreshing 
+function userSaved () {
+   
+    $(".hour").each(function() {
+
+        var currHour = $(this).text();
+        var currDescription = localStorage.getItem(currHour);
+    
+        console.log(this);
+        console.log(currHour);   
+
+        if(currDescription !== null) {
+            $(this).siblings(".description").val(currDescription);
+        }
+    })
+}
+
+// lets call the functions 
+colorcode();
+userSaved();
+
+
+
+
+// INSTRUCTIONS
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
